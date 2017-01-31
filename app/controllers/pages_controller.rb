f27abab33926c9f69d2ffc8948767e7399c7fc63
noah tenser <ntenser@hotmail.com>
2017-01-31 09:33:46 -0500

    first

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,34 @@
+class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
+
+  def welcome
+    @header = "This is the welcome page"
+  end
+
+  def about
+  end
+
+  def contest
+    flash[:notice] = "Sorry, the contest has ended"
+    redirect_to "/welcome"
+  end
+
+  def kitten
+  end
+
+  def kittens
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+  def secrets
+    if params[:magic_word] == "abracadabra"
+      render :secrets
+    else
+      flash[:alert] = "Sorry, you're not authorized to see that page!"
+      redirect_to "/welcome"
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+root 'pages#welcome'
+get '/welcome' => 'pages#welcome'
+get '/about' => 'pages#about'
+get '/contest' => 'pages#contest'
+get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+get '/kittens/:size' => 'pages#kittens', as: 'kittens'
+get '/secrets/:magic_word' => 'pages#secrets', as: 'secrets'
+end
